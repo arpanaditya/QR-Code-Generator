67f1ccb013a164204ddfd895e46a21d7fae1b8cf
arpanaditya <adityaarpansahoo@gmail.com>
2023-04-08 02:24:17 +0530

    feat: built fully functional qr code generator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRCode from "./QRCode";
 
 function App() {
   return (
     <div className="App">
-    
+      <QRCode />
     </div>
   );
 }
---
+++ src/QRCode.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function QRCode() {
+  const [value, setvalue] = useState("HelloWorld");
+
+  const handleChange = (e) => {
+    setvalue(e.target.value);
+  };
+
+  const imgsrc = `https://api.qrserver.com/v1/create-qr-code/?data=${value}&size=200X200&margin=20&amp;size=200x200`;
+
+  return (
+    <div>
+    <div className="heading">
+      <h2>QR Code Generator</h2>
+      <p>Enter any text and generate the QR Code</p>
+    </div>
+      <div className="text-container">
+        <input
+          type="text"
+          name="textbox"
+          id="textbox"
+          placeholder="HelloWorld"
+          onChange={handleChange}
+          value={value}
+        />
+      </div>
+      <div className="img-container">
+        <img src={imgsrc} alt={value} title={value} />
+      </div>
+    </div>
+  );
+}
