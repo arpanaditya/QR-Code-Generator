fd5499dfd599e8b0d22fd53d483a8833b1dae30f
arpanaditya <adityaarpansahoo@gmail.com>
2023-04-08 02:42:35 +0530

    Updates

---
+++ precache-manifest.927b33d661f70f27a1f6091f066f33a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "475868cda2f5e039b389",
+    "url": "/qr-code-generator/static/css/main.82126cd7.chunk.css"
+  },
+  {
+    "revision": "475868cda2f5e039b389",
+    "url": "/qr-code-generator/static/js/main.475868cd.chunk.js"
+  },
+  {
+    "revision": "2c2be63aec83ef38e371",
+    "url": "/qr-code-generator/static/js/1.2c2be63a.chunk.js"
+  },
+  {
+    "revision": "016108d46e07c75f6eab",
+    "url": "/qr-code-generator/static/js/runtime~main.016108d4.js"
+  },
+  {
+    "revision": "541763f864f160929628b6133c64910f",
+    "url": "/qr-code-generator/index.html"
+  }
+];
